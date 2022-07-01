a90a6423b3aa2521e91c53acb5bd93f88e7bc591
Ty Hunt <ty@Ty.localdomain>
2022-06-30 21:59:14 -0600

    I need to extract a couple more things

--- scraper.py
+++ scraper.py
@@ -15,12 +15,12 @@ soup = BeautifulSoup(page.content, "html.parser")
 #get the table that we want
 tables = soup.find_all('table')
 table = soup.find('table', class_="stats_table")
-a = []
 
 for row in table.tbody.find_all('tr'):
     columns = row.find_all('td')
-    for i in columns:
-        print(i.text)
 
+    if (columns != []):
+        players_name = columns[0].text.strip()
+        
 
 
